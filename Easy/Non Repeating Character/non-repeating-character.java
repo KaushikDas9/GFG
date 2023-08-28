a5edbea9741dc044fe7342fb28dc862b1463ce8e
KaushikDas9 <123364033+KaushikDas9@users.noreply.github.com>
2023-08-28 22:18:10 +0530

    Updated Solution - GfG to GitHub

--- Easy/Non Repeating Character/non-repeating-character.java
+++ Easy/Non Repeating Character/non-repeating-character.java
@@ -44,7 +44,7 @@ class Solution
         }
          for( int i=0;i<S.length();i++ ) {
              
-            if( map.get(S.charAt(i))==1 ) return S.charAt(i);
+            if(map.get(S.charAt(i))==1) return S.charAt(i);
         
         }
         return '$';
