439ca325c1e9475592d7fa8daea4ae1e22466f93
KaushikDas9 <123364033+KaushikDas9@users.noreply.github.com>
2023-08-29 11:30:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        // your code here
+         if(head==null || head.next == null)  return head;
+        Node ptr = head;
+        
+        while(ptr.next != null && ptr != null){
+            
+            if(ptr.data < ptr.next.data){
+                
+                ptr.data = ptr.next.data;
+                ptr.next = ptr.next.next;
+                ptr = head;
+            }
+            else    ptr = ptr.next;
+        }
+        return head;
+    }
+}
+  
