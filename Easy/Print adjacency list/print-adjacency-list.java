e9ee6c835e82b8b943778cace2066fe013967a38
KaushikDas9 <123364033+KaushikDas9@users.noreply.github.com>
2023-11-11 10:08:30 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print adjacency list/print-adjacency-list.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int edges[][] = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                s = in.readLine().trim().split(" ");
+                edges[i][0] = Integer.parseInt(s[0]);
+                edges[i][1] = Integer.parseInt(s[1]);
+            }
+            Solution ob = new Solution();
+            List<List<Integer>> A = ob.printGraph(n, edges);
+            for (var i : A) {
+                Collections.sort(i);
+                for (int j : i) {
+                    out.print(j + " ");
+                }
+                out.println();
+            }
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for ( int i=0 ;  i<V ; i++ ) { 
+            // ans.get(i).add(new ArrayList<>());
+            ans.add(i ,new ArrayList<>() );
+        }
+        
+        for ( int i=0 ;  i<edges.length ; i++ ) { 
+            int index1 = edges[i][0];
+            // ans.add(index1 , edges[i][1]);
+            ans.get(index1).add(edges[i][1]);
+            
+            int index2 = edges[i][1];
+            // ans.add(index2 , edges[i][0]);
+            ans.get(index2).add(edges[i][0]);
+        }
+        
+        return ans;
+    }
+}
