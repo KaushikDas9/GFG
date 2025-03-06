035816ff9d7675e985cd5b06e390f4f34676c9a5
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-07 01:50:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find length of the longest subarray containing atmost two distinct integers/find-length-of-the-longest-subarray-containing-atmost-two-distinct-integers.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            // Convert ArrayList to array
+            Integer[] arr = new Integer[array.size()];
+            array.toArray(arr);
+
+            Solution ob = new Solution();
+
+            // Call totalFruits method and print result
+            Integer ans = ob.totalElements(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static int totalElements(Integer[] arr) {
+            
+        int n=arr.length;
+        int ans =0;
+        for ( int i=0;i<n;i++ ) {
+            
+            HashSet<Integer> set = new HashSet<>();
+            for( int j=i;j<n;j++ ) { 
+                
+                set.add(arr[j]);
+                if( set.size()<=2 ) { 
+                 ans=Math.max(ans,j-i+1);
+                } else break;
+            }
+        }
+        
+        return ans;
+    }
+}
