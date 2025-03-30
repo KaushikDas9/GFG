7b441d100c9ccbfad343471198c7e7944531d607
Koushik Das <123364033+KaushikDas9@users.noreply.github.com>
2025-03-30 16:15:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -52,33 +52,38 @@ public class Main {
 class Solution {
     public int minCost(int[] height) {
         int n = height.length;
-        if (n == 0) return 0;
-
-        // Initialize the memoization table with -1 (uncomputed)
+        if (n == 1) return 0;
+        
         int[] dp = new int[n];
-        for( int i=0;i<n;i++ ) dp[i]=-1;
 
-        // Start the recursion from the last stair
-        return findMinCost(height, n - 1, dp);
+        // return findMinCost(height, n - 1, dp
+        
+        dp[0]=0;dp[1]= Math.abs(height[1] - height[0]);
+            
+        for ( int i=2;i<n;i++ ) { 
+            int l =  Math.abs(height[i] - height[i-1]) + dp[i-1];
+            int r =  Math.abs(height[i] - height[i-2]) + dp[i-2];
+            dp[i]=Math.min(l,r);
+        }
+        
+        return dp[n-1];
     }
 
     private int findMinCost(int[] height, int i, int[] dp) {
-        // Base case: If we are at the first stair, the cost is 0
+        
         if (i == 0) return 0;
 
-        // If we are at the second stair, the cost is the absolute difference between the first and second stair
         if (i == 1) return Math.abs(height[1] - height[0]);
 
-        // If the result is already computed, return it
+
         if (dp[i] != -1) return dp[i];
 
-        // Calculate the cost of jumping from (i-1)th stair
         int jump1 = findMinCost(height, i - 1, dp) + Math.abs(height[i] - height[i - 1]);
 
-        // Calculate the cost of jumping from (i-2)th stair
+
         int jump2 = findMinCost(height, i - 2, dp) + Math.abs(height[i] - height[i - 2]);
 
-        // Store the minimum cost in the memoization table
+
         dp[i] = Math.min(jump1, jump2);
 
         return dp[i];
